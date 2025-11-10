f092c3602f2d41dc098b49f2af670819bad8df41
redkrayn <redkrayn@yandex.ru>
2025-11-10 22:55:20 +0800

    Add logging

--- telegram_helper_bot.py
+++ telegram_helper_bot.py
@@ -1,6 +1,35 @@
+import logging
+
 from environs import Env
 from google.cloud import dialogflow
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
+def setup_logging(tg_bot=None, chat_id=None):
+    logger = logging.getLogger('tg_verb_bot')
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        '%(message)s'
+    )
+
+    tg_handler = TelegramLogsHandler(tg_bot, chat_id)
+    tg_handler.setLevel(logging.INFO)
+    tg_handler.setFormatter(formatter)
+    logger.addHandler(tg_handler)
+
+    return logger
 
 
 def start(update, context):
@@ -23,29 +52,41 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
 
 
 def echo(update, context):
-    user_text = update.message.text
-    chat_id = update.message.chat.id
+    logger = context.bot_data['logger']
+    try:
+        user_text = update.message.text
+        chat_id = update.message.chat.id
 
-    response_text = detect_intent_texts(
-        project_id='verb-helper',
-        session_id=str(chat_id),
-        texts=[user_text],
-        language_code="ru"
-    )
+        response_text = detect_intent_texts(
+            project_id='verb-helper',
+            session_id=str(chat_id),
+            texts=[user_text],
+            language_code="ru"
+        )
 
-    update.message.reply_text(response_text)
+        update.message.reply_text(response_text)
+
+    except Exception as e:
+        logger.error(f"Бот упал с ошибкой: {e}", exc_info=True)
 
 
 def main():
     env = Env()
     env.read_env()
+
     telegram_bot_token = env('TELEGRAM_BOT_TOKEN')
+    telegram_chat_id = env('TELEGRAM_CHAT_ID')
 
     updater = Updater(telegram_bot_token)
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    logger = setup_logging(updater.bot, telegram_chat_id)
+    updater.dispatcher.bot_data['logger'] = logger
+
+    logger.info('Бот запущен')
 
     updater.start_polling()
     updater.idle()
--- vk_helper_bot.py
+++ vk_helper_bot.py
@@ -1,9 +1,38 @@
 import random
-
+import logging
+import telegram
 import vk_api as vk
+
 from environs import Env
 from google.cloud import dialogflow
 from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
+def setup_logging(tg_bot=None, chat_id=None):
+    logger = logging.getLogger('vk_verb_bot')
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        '%(message)s'
+    )
+
+    telegram_handler = TelegramLogsHandler(tg_bot, chat_id)
+    telegram_handler.setLevel(logging.INFO)
+    telegram_handler.setFormatter(formatter)
+    logger.addHandler(telegram_handler)
+
+    return logger
 
 
 def detect_intent_texts(project_id, session_id, texts, language_code):
@@ -20,31 +49,43 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
     return response.query_result
 
 
-def echo(event, vk_api):
-    dialogflow_response = detect_intent_texts(
-        project_id='verb-helper',
-        session_id=str(event.user_id),
-        texts=[event.text],
-        language_code='ru'
-    )
-
-    if not dialogflow_response.intent.is_fallback:
-        vk_api.messages.send(
-            user_id=event.user_id,
-            message=dialogflow_response.fulfillment_text,
-            random_id=random.randint(1, 1000)
+def echo(event, vk_api, logger):
+    try:
+        dialogflow_response = detect_intent_texts(
+            project_id='verb-helper',
+            session_id=str(event.user_id),
+            texts=[event.text],
+            language_code='ru'
         )
+
+        if not dialogflow_response.intent.is_fallback:
+            vk_api.messages.send(
+                user_id=event.user_id,
+                message=dialogflow_response.fulfillment_text,
+                random_id=random.randint(1, 1000)
+            )
+
+    except Exception as e:
+        logger.error(f"Бот упал с ошибкой: {e}", exc_info=True)
 
 
 if __name__ == "__main__":
     env = Env()
     env.read_env()
+
     vk_token = env('VK_TOKEN')
+    telegram_chat_id = env('TELEGRAM_CHAT_ID')
+    telegram_bot_token = env('TELEGRAM_BOT_TOKEN')
 
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
+    tg_bot = telegram.Bot(token=telegram_bot_token)
+
+    logger = setup_logging(tg_bot, telegram_chat_id)
+    logger.info('Бот запущен')
+
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            echo(event, vk_api, logger)
