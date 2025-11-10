9115f7e6fa24f8408a6e70a9fc10d0fcd3bbde9e
redkrayn <redkrayn@yandex.ru>
2025-11-10 21:35:03 +0800

    echobot for VK.

--- vk_helper_bot.py
+++ vk_helper_bot.py
@@ -1,20 +1,25 @@
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+import random
+
+import vk_api as vk
 from environs import Env
+from vk_api.longpoll import VkLongPoll, VkEventType
 
-env = Env()
-env.read_env()
 
-vk_token = env('VK_TOKEN')
-vk_session = vk_api.VkApi(token=vk_token)
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1, 1000)
+    )
 
-longpoll = VkLongPoll(vk_session)
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        print('Новое сообщение:')
-        if event.to_me:
-            print('Для меня от: ', event.user_id)
-        else:
-            print('От меня для: ', event.user_id)
-        print('Текст:', event.text)
+if __name__ == "__main__":
+    env = Env()
+    env.read_env()
+    vk_token = env('VK_TOKEN')
+    vk_session = vk.VkApi(token=vk_token)
+    vk_api = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
