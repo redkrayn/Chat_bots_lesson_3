4d34f447464e872acdeb8bf6e811ce3b53efdd74
redkrayn <redkrayn@yandex.ru>
2025-11-10 21:47:28 +0800

    add DialogFlow to VK bot.

--- vk_helper_bot.py
+++ vk_helper_bot.py
@@ -2,13 +2,35 @@ import random
 
 import vk_api as vk
 from environs import Env
+from google.cloud import dialogflow
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    for text in texts:
+        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+        query_input = dialogflow.QueryInput(text=text_input)
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
+        )
+
+    return response.query_result.fulfillment_text
+
+
 def echo(event, vk_api):
+    dialogflow_response = detect_intent_texts(
+        project_id='verb-helper',
+        session_id=str(event.user_id),
+        texts=[event.text],
+        language_code='ru'
+    )
+
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=dialogflow_response,
         random_id=random.randint(1, 1000)
     )
 
@@ -17,9 +39,11 @@ if __name__ == "__main__":
     env = Env()
     env.read_env()
     vk_token = env('VK_TOKEN')
+
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
+
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             echo(event, vk_api)
